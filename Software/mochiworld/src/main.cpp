32fbc42e95f9222bc7caeb44087a051708f6fa2b
mochicraft01 <skmochicraft01@gmail.com>
2022-09-02 17:19:15 +0900

    ジャイロ

---
+++ Software/mochiworld/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+void irisawa();
+void setup() {
+  // put your setup code here, to run once:
+  irisawa();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+void irisawa() {
+
+}
