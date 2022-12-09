db7ba19c3ae5fd24e4b74514bea9a3428422c6c4
mochicraft01 <skmochicraft01@gmail.com>
2022-12-09 21:07:09 +0900

    PIDパラメータいじり　はじめました

--- Software/Robot Gen 2/Pico Main/src/main.cpp
+++ Software/Robot Gen 2/Pico Main/src/main.cpp
@@ -7,7 +7,10 @@
 
 // Line line;
 SerialPIO motor(22, 16, 32);
+//SerialPIO Serial1(0, 1, 32);
 Gyro gyro;
+
+float ir_angle = 0.0;
 
 void setup() {
     // put your setup code here, to run once:
@@ -29,14 +32,32 @@ void loop() {
     //  motor.write(100);
 }
 
-void setup1() { motor.begin(115200); }
+void setup1() {
+    motor.begin(115200);
+    Serial1.begin(9600);
+}
 
 void loop1() {
+    if (Serial1.available() > 2) {
+        int recv_data = Serial1.read();
+        if (recv_data == 255) {
+            int data[2];
+            data[0] = Serial1.read();
+            data[1] = Serial1.read();
+            int a = data[0] + (data[1] << 8);
+            ir_angle = (a / 100.0) - PI;
+        }
+        Serial.println(ir_angle);
+    }
+
     int a = (gyro.angle + PI) * 100;
-    byte data[2];
+    int b = (ir_angle + PI) * 100;
+    byte data[4];//[0][1]:gyro, [2][3]:ir
     data[0] = byte(a);
     data[1] = byte(a >> 8);
+    data[2] = byte(b);
+    data[3] = byte(b >> 8);
     motor.write(255);
-    motor.write(data, 2);
+    motor.write(data, 4);
     delay(10);
 }
--- Software/Robot Gen 2/XIAO RP2040 Motor/src/main.cpp
+++ Software/Robot Gen 2/XIAO RP2040 Motor/src/main.cpp
@@ -1,17 +1,18 @@
 #include <Arduino.h>
 
 #define PWM_FREQ 80000
-#define Kp 50.0
+#define Kp 25.0
 #define Kd 1.0
 #define Ki 10.0
 
 const int MOTOR[4][2] = {{D0, D1}, {D2, D3}, {D4, D5}, {D8, D9}};
-const float MOTOR_POS[4] = {PI / 4.0, 3.0 * PI / 4.0, 5.0 * PI / 4.0,
-                            7.0 * PI / 4.0};
+const float MOTOR_POS[4] = {3.0 * PI / 4.0, PI / 4.0, 7.0 * PI / 4.0,
+                            5.0 * PI / 4.0};
 
 float gyro_angle = 0.0;
+float ir_angle = 0.0;
 unsigned long long pre_time = 0;
-float pre_angle = 0.0;
+float pre_gyro_angle = 0.0;
 float I = 0.0;
 
 void cal(float dir, int speed);
@@ -31,8 +32,17 @@ void setup() {
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    cal(0, 0);
+    cal(PI/2, 50);
+    /*
+    cal(0, 50);
+    delay(2000);
+    cal(PI/2, 50);
+    delay(2000);
+    cal(PI, 50);
+    delay(2000);
+    cal(PI*3/2, 50);
+    delay(2000);
+    */
 }
 
 void cal(float dir, int speed) {
@@ -55,17 +65,17 @@ void cal(float dir, int speed) {
     unsigned long long now = micros();
     unsigned long long dt = (now - pre_time);
     P = gyro_angle * Kp;
-    D = (gyro_angle - pre_angle) / dt * Kd * 1000000.0;
-    if (abs(gyro_angle) < 0.0523598775) {
+    D = (gyro_angle - pre_gyro_angle) / dt * Kd * 1000000.0;
+    if (abs(gyro_angle) < radians(5)) {
         I = 0.0;
     }
     I += gyro_angle * dt * Ki / 1000000.0;
     PID = P + I + D;
     pre_time = now;
-    pre_angle = gyro_angle;
+    pre_gyro_angle = gyro_angle;
     for (int i = 0; i < 4; i++) {
         power[i] -= PID;
-        constrain(power[i], -128, 128);
+        constrain(power[i], -100, 100);
     }
     for (int i = 0; i < 4; i++) {
         Serial.print(power[i]);
@@ -79,14 +89,23 @@ void cal(float dir, int speed) {
 void setup1() { Serial1.begin(115200); }
 
 void loop1() {
-    if (Serial1.available() > 2) {
+    if (Serial1.available() > 4) {
         int recv_data = Serial1.read();
         if (recv_data == 255) {
-            int data[2];
+            int data[4];//[0][1]:gyro, [2][3]:ir
             data[0] = Serial1.read();
             data[1] = Serial1.read();
             int a = data[0] + (data[1] << 8);
             gyro_angle = (a / 100.0) - PI;
+            data[2] = Serial1.read();
+            data[3] = Serial1.read();
+            int b = data[2] + (data[3] << 8);
+            ir_angle = (b / 100.0) - PI;
         }
+        /*
+        Serial.print(gyro_angle);
+        Serial.print("\t");
+        Serial.println(ir_angle);
+        */
     }
 }
