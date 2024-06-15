a3c9abee8e07b7f291f4ade60beb837442dd2bf7
koji1027 <nkoji@g.ecc.u-tokyo.ac.jp>
2024-06-15 14:59:12 +0900

    FWのライン処理にコメントを追加

--- Software/Pico Main FW/src/line.cpp
+++ Software/Pico Main FW/src/line.cpp
@@ -1,197 +1,157 @@
 #include "line.h"
 
-void Line::begin()
-{
-        for (uint8_t i = 0; i < 2; i++)
-        {
-                pinMode(COM_PIN[i], INPUT);
-                for (uint8_t j = 0; j < 4; j++)
-                {
-                        pinMode(SELECT_PIN[i][j], OUTPUT);
-                }
+void Line::begin() {  // ラインセンサの初期化
+    // ２個アナログマルチプレクサ(CD74HC4067)を使うためのピン設定
+    for (uint8_t i = 0; i < 2; i++) {
+        pinMode(COM_PIN[i], INPUT);
+        for (uint8_t j = 0; j < 4; j++) {
+            pinMode(SELECT_PIN[i][j], OUTPUT);
         }
-        for (uint8_t i = 0; i < SENSOR_NUM; i++)
-        {
-                SENSOR_THETA[i] = (float)i * PI / 16.0;
-        }
-        for (uint8_t i = 0; i < SENSOR_NUM; i++)
-        {
-                SENSOR_VECTOR[i][0] = sin(SENSOR_THETA[i]);
-                SENSOR_VECTOR[i][1] = cos(SENSOR_THETA[i]);
-        }
-        for (uint8_t i = 0; i < SENSOR_NUM; i++)
-        {
-                threshold[i] = DEFAULT_THRESHOLD;
-        }
+    }
+    // センサの角度とベクトルを設定
+    for (uint8_t i = 0; i < SENSOR_NUM; i++) {
+        SENSOR_THETA[i] = (float)i * PI / 16.0;
+    }
+    for (uint8_t i = 0; i < SENSOR_NUM; i++) {
+        SENSOR_VECTOR[i][0] = sin(SENSOR_THETA[i]);
+        SENSOR_VECTOR[i][1] = cos(SENSOR_THETA[i]);
+    }
+    for (uint8_t i = 0; i < SENSOR_NUM; i++) {  // しきい値の初期化
+        threshold[i] = DEFAULT_THRESHOLD;
+    }
 }
 
-void Line::read()
-{
-        pre_line_theta = line_theta;
-        line_theta = 0.0;
-        for (int i = 0; i < 16; i++)
-        {
-                for (int j = 0; j < 4; j++)
-                {
-                        digitalWrite(SELECT_PIN[0][j], (byte)i & (1 << j));
-                        digitalWrite(SELECT_PIN[1][j], (byte)i & (1 << j));
-                }
-                sensor_value[i + 16] = analogRead(COM_PIN[0]);
-                sensor_value[i] = analogRead(COM_PIN[1]);
+void Line::read() {
+    pre_line_theta = line_theta;  // 前回のラインの角度を保存
+    line_theta = 0.0;             // ラインの角度を初期化
+    // 各ラインセンサの値の読み取り
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 4; j++) {
+            digitalWrite(SELECT_PIN[0][j], (byte)i & (1 << j));
+            digitalWrite(SELECT_PIN[1][j], (byte)i & (1 << j));
         }
+        sensor_value[i + 16] = analogRead(COM_PIN[0]);
+        sensor_value[i] = analogRead(COM_PIN[1]);
+    }
 
-        int posILW[SENSOR_NUM] = {0}; // 白線上にあるセンサの番号を格納
-        int numILW = 0;               // 白線上にあるセンサの数
-        for (int i = 0; i < SENSOR_NUM; i++)
-        {
-                if (sensor_value[i] > threshold[i])
-                {
-                        sensor_state[i] = true;
-                        posILW[numILW] = i;
-                        numILW++;
-                }
-                else
-                {
-                        sensor_state[i] = false;
-                }
+    // 白線の角度を計算する．（アルゴリズムについては"http://yuta.techblog.jp/archives/24145402.html"を参照）(多分これを使った)
+    int posILW[SENSOR_NUM] = {0};  // 白線上にあるセンサの番号を格納
+    int numILW = 0;                // 白線上にあるセンサの数
+    for (int i = 0; i < SENSOR_NUM; i++) {
+        if (sensor_value[i] > threshold[i]) {
+            sensor_state[i] = true;
+            posILW[numILW] = i;
+            numILW++;
+        } else {
+            sensor_state[i] = false;
         }
-        sensor_value[12] = 0;
-        sensor_state[12] = 0;
-        cluster_num = -1; // (実際の数) -１
-        int cluster[SENSOR_NUM] = {0};
-        int cluster_size[SENSOR_NUM] = {0};
-        for (int i = 0; i < SENSOR_NUM; i++)
-        {
-                if (sensor_state[i])
-                {
-                        if (cluster_num == -1)
-                        {
-                                cluster_num = 0;
-                                cluster[cluster_num] = i;
-                                cluster_size[cluster_num] = 1;
-                        }
-                        else if (cluster[cluster_num] + cluster_size[cluster_num] == i)
-                        {
-                                cluster_size[cluster_num]++;
-                        }
-                        else
-                        {
-                                cluster_num++;
-                                cluster[cluster_num] = i;
-                                cluster_size[cluster_num] = 1;
-                        }
-                }
+    }
+    sensor_value[12] = 0;
+    sensor_state[12] = 0;
+    cluster_num = -1;  // (実際の数) -１
+    int cluster[SENSOR_NUM] = {0};
+    int cluster_size[SENSOR_NUM] = {0};
+    for (int i = 0; i < SENSOR_NUM; i++) {
+        if (sensor_state[i]) {
+            if (cluster_num == -1) {
+                cluster_num = 0;
+                cluster[cluster_num] = i;
+                cluster_size[cluster_num] = 1;
+            } else if (cluster[cluster_num] + cluster_size[cluster_num] == i) {
+                cluster_size[cluster_num]++;
+            } else {
+                cluster_num++;
+                cluster[cluster_num] = i;
+                cluster_size[cluster_num] = 1;
+            }
         }
-        if (cluster_num == -1)
-        {
-                on_line = false;
-                return;
+    }
+    if (cluster_num == -1) {
+        on_line = false;
+        return;
+    } else {
+        on_line = true;
+        if (cluster[0] == 0 &&
+            cluster[cluster_num] + cluster_size[cluster_num] == SENSOR_NUM) {
+            cluster[0] = cluster[cluster_num];
+            cluster_size[0] += cluster_size[cluster_num];
+            cluster_num--;
         }
-        else
-        {
-                on_line = true;
-                if (cluster[0] == 0 &&
-                    cluster[cluster_num] + cluster_size[cluster_num] == SENSOR_NUM)
-                {
-                        cluster[0] = cluster[cluster_num];
-                        cluster_size[0] += cluster_size[cluster_num];
-                        cluster_num--;
-                }
-                float cluster_theta[cluster_num + 1] = {0.0};
-                for (int i = 0; i <= cluster_num; i++)
-                {
-                        if (cluster_size[i] % 2)
-                        {
-                                cluster_theta[i] =
-                                    SENSOR_THETA[cluster[i] + (cluster_size[i] - 1) / 2];
-                        }
-                        else
-                        {
-                                cluster_theta[i] =
-                                    SENSOR_THETA[cluster[i] + cluster_size[i] / 2] - PI / 32.0;
-                        }
-                }
-                float sum_vector[2] = {0.0, 0.0};
-                for (int i = 0; i <= cluster_num; i++)
-                {
-                        sum_vector[0] += sin(cluster_theta[i]);
-                        sum_vector[1] += cos(cluster_theta[i]);
-                }
-                line_theta = atan2(sum_vector[0], sum_vector[1]);
+        float cluster_theta[cluster_num + 1] = {0.0};
+        for (int i = 0; i <= cluster_num; i++) {
+            if (cluster_size[i] % 2) {
+                cluster_theta[i] =
+                    SENSOR_THETA[cluster[i] + (cluster_size[i] - 1) / 2];
+            } else {
+                cluster_theta[i] =
+                    SENSOR_THETA[cluster[i] + cluster_size[i] / 2] - PI / 32.0;
+            }
         }
+        float sum_vector[2] = {0.0, 0.0};
+        for (int i = 0; i <= cluster_num; i++) {
+            sum_vector[0] += sin(cluster_theta[i]);
+            sum_vector[1] += cos(cluster_theta[i]);
+        }
+        line_theta = atan2(sum_vector[0], sum_vector[1]);
+    }
 }
 
-void Line::set_threshold()
-{
-        int maxValue[32];
-        int minValue[32];
-        for (int i = 0; i < 32; i++)
-        {
-                maxValue[i] = 0;
-                minValue[i] = 1023;
+void Line::set_threshold() {  // しきい値の自動設定
+    int maxValue[32];
+    int minValue[32];
+    for (int i = 0; i < 32; i++) {
+        maxValue[i] = 0;
+        minValue[i] = 1023;
+    }
+    bool loop_flag = true;
+    while (loop_flag) {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 4; j++) {
+                digitalWrite(SELECT_PIN[0][j], (byte)i & (1 << j));
+                digitalWrite(SELECT_PIN[1][j], (byte)i & (1 << j));
+            }
+            int value0 = analogRead(COM_PIN[0]);
+            int value1 = analogRead(COM_PIN[1]);
+            if (value0 > maxValue[i]) {
+                maxValue[i] = value0;
+            }
+            if (value0 < minValue[i]) {
+                minValue[i] = value0;
+            }
+            if (value1 > maxValue[i + 16]) {
+                maxValue[i + 16] = value1;
+            }
+            if (value1 < minValue[i + 16]) {
+                minValue[i + 16] = value1;
+            }
         }
-        bool loop_flag = true;
-        while (loop_flag)
-        {
-                for (int i = 0; i < 16; i++)
-                {
-                        for (int j = 0; j < 4; j++)
-                        {
-                                digitalWrite(SELECT_PIN[0][j], (byte)i & (1 << j));
-                                digitalWrite(SELECT_PIN[1][j], (byte)i & (1 << j));
-                        }
-                        int value0 = analogRead(COM_PIN[0]);
-                        int value1 = analogRead(COM_PIN[1]);
-                        if (value0 > maxValue[i])
-                        {
-                                maxValue[i] = value0;
-                        }
-                        if (value0 < minValue[i])
-                        {
-                                minValue[i] = value0;
-                        }
-                        if (value1 > maxValue[i + 16])
-                        {
-                                maxValue[i + 16] = value1;
-                        }
-                        if (value1 < minValue[i + 16])
-                        {
-                                minValue[i + 16] = value1;
-                        }
-                }
-                if (digitalRead(D19) == LOW)
-                {
-                        loop_flag = false;
-                }
+        if (digitalRead(D19) == LOW) {
+            loop_flag = false;
         }
-        for (int i = 0; i < 32; i++)
-        {
-                threshold[i] = (float)(maxValue[i] + minValue[i]) / 2.0;
-        }
-        Serial.println("自動しきい値設定完了！");
-        for (int i = 0; i < 32; i++)
-        {
-                Serial.print(threshold[i]);
-                Serial.print("\t");
-                ave_threshold += threshold[i];
-        }
-        ave_threshold = (float)ave_threshold / (float)SENSOR_NUM;
-        Serial.println();
+    }
+    for (int i = 0; i < 32; i++) {
+        threshold[i] = (float)(maxValue[i] + minValue[i]) / 2.0;
+    }
+    Serial.println("自動しきい値設定完了！");
+    for (int i = 0; i < 32; i++) {
+        Serial.print(threshold[i]);
+        Serial.print("\t");
+        ave_threshold += threshold[i];
+    }
+    ave_threshold = (float)ave_threshold / (float)SENSOR_NUM;
+    Serial.println();
 }
 
-void Line::debug()
-{
-        for (int i = 0; i < 16; i++)
-        {
-                Serial.print(sensor_value[i]);
-                Serial.print("\t");
-        }
-        Serial.println();
-        for (int i = 16; i < 32; i++)
-        {
-                Serial.print(sensor_value[i]);
-                Serial.print("\t");
-        }
-        Serial.println();
-        Serial.println();
+void Line::debug() {  // デバッグ用の関数
+    for (int i = 0; i < 16; i++) {
+        Serial.print(sensor_value[i]);
+        Serial.print("\t");
+    }
+    Serial.println();
+    for (int i = 16; i < 32; i++) {
+        Serial.print(sensor_value[i]);
+        Serial.print("\t");
+    }
+    Serial.println();
+    Serial.println();
 }
--- Software/Pico Main FW/src/line.h
+++ Software/Pico Main FW/src/line.h
@@ -2,33 +2,32 @@
 #define LINE_H
 #include <Arduino.h>
 
-#define SENSOR_NUM 32
-#define LINE_SENSOR_RADIUS 53.0
-#define DEFAULT_THRESHOLD 700
+#define SENSOR_NUM 32            // センサの数
+#define LINE_SENSOR_RADIUS 53.0  // センサの半径(mm)
+#define DEFAULT_THRESHOLD 700    // デフォルトのしきい値
 
-class Line
-{
-public:
-        void begin();
-        void read();
-        void debug();
-        void set_threshold();
-        int on_line = false;
-        bool floor_flag = false;
-        double line_theta = 0.0;
-        double pre_line_theta = 0.0;
-        int cluster_num = -1; // 実際の数-１
-        int line_state_flag = 0;
-        uint16_t ave_threshold = DEFAULT_THRESHOLD;
+class Line {
+   public:
+    void begin();                                // ラインセンサの初期化
+    void read();                                 // ラインセンサの値の読み取り
+    void debug();                                // デバッグ用
+    void set_threshold();                        // しきい値の設定
+    int on_line = false;                         // ライン上にあるかどうか
+    bool floor_flag = false;                     // 床に機体が接地しているかどうか
+    double line_theta = 0.0;                     // ラインの角度
+    double pre_line_theta = 0.0;                 // 1ループ前のラインの角度
+    int cluster_num = -1;                        // 実際の数-１(自分でもよくわからない)
+    int line_state_flag = 0;                     // ラインの状態
+    uint16_t ave_threshold = DEFAULT_THRESHOLD;  // しきい値の平均値
 
-private:
-        const int COM_PIN[2] = {A1, A0};
-        const int SELECT_PIN[2][4] = {{D8, D9, D10, D11}, {D2, D3, D6, D7}};
-        double SENSOR_THETA[SENSOR_NUM] = {0.0};
-        double SENSOR_VECTOR[SENSOR_NUM][2] = {0.0};
-        uint16_t threshold[SENSOR_NUM] = {0};
-        uint16_t sensor_value[SENSOR_NUM] = {0};
-        bool sensor_state[SENSOR_NUM] = {false};
+   private:
+    const int COM_PIN[2] = {A1, A0};                                      // マルチプレクサのCOMピン
+    const int SELECT_PIN[2][4] = {{D8, D9, D10, D11}, {D2, D3, D6, D7}};  // マルチプレクサのセレクトピン
+    double SENSOR_THETA[SENSOR_NUM] = {0.0};                              // センサの角度
+    double SENSOR_VECTOR[SENSOR_NUM][2] = {0.0};                          // センサのベクトル
+    uint16_t threshold[SENSOR_NUM] = {0};                                 // しきい値
+    uint16_t sensor_value[SENSOR_NUM] = {0};                              // センサの値
+    bool sensor_state[SENSOR_NUM] = {false};                              // センサの状態
 };
 
 #endif
